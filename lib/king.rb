955fb080e08f51f999cc5061a3eff4179bb03110
icarus2176 <ashe.kemuri@gmail.com>
2023-04-22 18:58:13 -0500

    create method that finds available moves for king

--- lib/king.rb
+++ lib/king.rb
@@ -3,7 +3,7 @@ require_relative "piece"
 class King < Piece
   attr_accessor :x, :y
   attr_reader :color, :moves, :board, :symbol
-
+  move = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 0], [0, 1], [1, -1], [1, 0], [1, 1]]
   def initialize(x, y, color, board)
     super
     if color
@@ -11,5 +11,15 @@ class King < Piece
     else
       @symbol = "🨀"
     end
+  end
+
+  def moves_available 
+    moves = []
+    move.each do |space|
+      new_x = @x + space[0]
+      new_y = @y + space[1]
+      moves.push([new_x, new_y]) if new_x.between?(0, 7) && new_y.between?(0, 7)
+    end
+    @moves = moves
   end
 end
