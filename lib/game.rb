2a8db7468d951eb91993e14d954c7b32869b832e
icarus2176 <ashe.kemuri@gmail.com>
2023-04-21 19:17:52 -0500

    change create methods to make piece list

--- lib/game.rb
+++ lib/game.rb
@@ -9,6 +9,8 @@ require_relative "king"
 class Game
   def initialize
     @board = Board.new
+    @white_pieces = []
+    @black_pieces = []
     create_pieces
   end
 
@@ -25,47 +27,87 @@ class Game
     @board.spaces[1].each_with_index do |space, index|
       pawn = Pawn.new(index, 1, true)
       space.piece = pawn
+      @black_pieces.push(pawn)
     end
 
     @board.spaces[6].each_with_index do |space, index|
       pawn = Pawn.new(index, 6, false)
       space.piece = pawn
+      @white_pieces.push(pawn)
     end
   end
 
   def create_rooks
-    @board.spaces[0, 0].piece = Rook.new(0, 0, true)
-    @board.spaces[0, 7].piece = Rook.new(0, 7, true)
+    rook1 = Rook.new(0, 0, true)
+    @board.spaces[0, 0].piece = rook1
+    @black_pieces.push(rook1)
 
-    @board.spaces[7, 0].piece = Rook.new(7, 0, false)
-    @board.spaces[7, 7].piece = Rook.new(7, 7, false)
+    rook2 = Rook.new(0, 7, true)
+    @board.spaces[0, 7].piece = rook2
+    @black_pieces.push(rook1)
+
+    rook3 = Rook.new(7, 0, false)
+    @board.spaces[7, 0].piece = rook3
+    @white_pieces.push(rook3)
+
+    rook4 = Rook.new(7, 7, false)
+    @board.spaces[7, 7].piece = rook4
+    @white_pieces.push(rook4)
   end
 
   def create_knights
-    @board.spaces[0, 1].piece = Knight.new(0, 1, true)
-    @board.spaces[0, 6].piece = Knight.new(0, 6, true)
+    knight1 = Knight.new(0, 1, true)
+    @board.spaces[0, 1].piece = knight1
+    @black_pieces.push(knight1)
 
-    @board.spaces[7, 1].piece = Knight.new(7, 1, false)
-    @board.spaces[7, 6].piece = Knight.new(7, 6, false)
+    knight2 = Knight.new(0, 6, true)
+    @board.spaces[0, 6].piece = knight2
+    @black_pieces.push(knight2)
+
+    knight3 = Knight.new(7, 1, false)
+    @board.spaces[7, 1].piece = knight3
+    @white_pieces.push(knight3)
+
+    knight4 = Knight.new(7, 6, false)
+    @board.spaces[7, 6].piece = knight4
+    @white_pieces.push(knight4)
   end
 
   def create_bishops
-    @board.spaces[0, 2].piece = Bishop.new(0, 2, true)
-    @board.spaces[0, 5].piece = Bishop.new(0, 5, true)
+    bishop1 = Bishop.new(0, 2, true)
+    @board.spaces[0, 2].piece = bishop1
+    @black_pieces.push(bishop1)
 
-    @board.spaces[7, 2].piece = Bishop.new(7, 2, false)
-    @board.spaces[7, 5].piece = Bishop.new(7, 5, false)
+    bishop2 = Bishop.new(0, 5, true)
+    @board.spaces[0, 5].piece = bishop2
+    @black_pieces.push(bishop2)
+
+    bishop3 = Bishop.new(7, 2, false)
+    @board.spaces[7, 2].piece = bishop3
+    @white_pieces.push(bishop3)
+
+    bishop4 = Bishop.new(7, 5, false)
+    @board.spaces[7, 5].piece = bishop4
+    @white_pieces.push(bishop4)
   end
 
   def create_queens
-    @board.spaces[0, 0].piece = Queen.new(0, 4, true)
+    queen1 = Queen.new(0, 4, true)
+    @board.spaces[0, 0].piece = queen1
+    @black_pieces.push(queen1)
 
-    @board.spaces[7, 7].piece = Queen.new(7, 4, false)
+    queen2 = Queen.new(7, 4, false)
+    @board.spaces[7, 7].piece = queen2 
+    @white_pieces.push(queen2)
   end
 
   def create_kings
-    @board.spaces[0, 3].piece = King.new(0, 3, true)
+    king1 = King.new(0, 3, true)
+    @board.spaces[0, 3].piece = king1
+    @black_pieces.push(king1)
 
-    @board.spaces[7, 3].piece = King.new(7, 3, false)
+    king2 = King.new(7, 3, false)
+    @board.spaces[7, 3].piece = king2
+    @@white_pieces.push(king2)
   end
 end
