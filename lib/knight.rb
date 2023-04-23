6aee138b36f45774ab123f0549b08a6167b78187
icarus2176 <ashe.kemuri@gmail.com>
2023-04-22 19:02:09 -0500

    create method that finds available moves for knight

--- lib/knight.rb
+++ lib/knight.rb
@@ -3,6 +3,7 @@ require_relative "piece"
 class Knight < Piece
   attr_accessor :x, :y
   attr_reader :color, :moves, :board, :symbol
+  moves = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]]
 
   def initialize(x, y, color, board)
     super
@@ -11,5 +12,15 @@ class Knight < Piece
     else
       @symbol = "🨄"
     end
+  end
+
+  def moves_available 
+    moves = []
+    move.each do |space|
+      new_x = @x + space[0]
+      new_y = @y + space[1]
+      moves.push([new_x, new_y]) if new_x.between?(0, 7) && new_y.between?(0, 7)
+    end
+    @moves = moves
   end
 end
